ebcbfe5eda25b9a481e37e2214cba22f7917255e
Tyler Chang <chang.ty97@gmail.com>
2021-06-02 18:01:46 -0700

    Updated es linting errors

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <HashRouter>
         <div>
           <div className="image-nav">
-            <img className="circular--square center" src="/headshot2.jpg" />
+            <img alt="Tyler Headshot Image" className="circular--square center" src="/headshot2.jpg" />
             <h1 className="center">Tyler Chang</h1>
           </div>
           <hr></hr>
@@ -23,9 +23,9 @@ function App() {
               <li><NavLink to="/">HOME</NavLink></li>
               <li><NavLink to="/resume">RESUME</NavLink></li>
               <li><NavLink to="/projects">PROJECTS</NavLink></li>
-              <li><a href="https://github.com/changty97" class="fa fa-github"></a></li>
-              <li><a href="https://www.linkedin.com/in/tyler-chang" class="fa fa-linkedin"></a></li>
-              <li><a href="mailto: chang.ty97@gmail.com" class="fa fa-google"></a></li>
+              <li><a href="https://github.com/changty97" class="fa fa-github"> </a></li>
+              <li><a href="https://www.linkedin.com/in/tyler-chang" class="fa fa-linkedin"> </a></li>
+              <li><a href="mailto: chang.ty97@gmail.com" class="fa fa-google"> </a></li>
             </ul>
           </div>
           <div className="content">
--- src/Resume.js
+++ src/Resume.js
@@ -5,7 +5,7 @@ class Resume extends Component {
     return (
       <div className="center">
         <h2>RESUME</h2>
-        <iframe src="/Tyler_Chang_Resume_2021.pdf" className="resume" frameborder="0" height="680" width="500"></iframe>
+        <iframe src="/Tyler_Chang_Resume_2021.pdf" title="Resume PDF file" className="resume" frameborder="0" height="680" width="500"></iframe>
       </div>
     );
   }
