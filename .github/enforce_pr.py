08495730e650ded861c1f6b1357c5302d490ca52
Tyler <43707450+changty97@users.noreply.github.com>
2022-11-08 15:07:45 -0800

    Update enforce_pr.py

--- .github/enforce_pr.py
+++ .github/enforce_pr.py
@@ -5,7 +5,7 @@ import regex
 
 pr_title = ["OVD", "FVT", "No-JIRA"]
 
-if regex.findall(r"(?=("+'|'.join(pr_title)+r"))", os.environ.get('PR_TITLE')):
-  print(os.environ.get('PR_TITLE'))
-else:
+if !regex.findall(r"(?=("+'|'.join(pr_title)+r"))", os.environ.get('PR_TITLE')):
+#   print(os.environ.get('PR_TITLE'))
+# else:
   sys.exit("Pull Request Title MUST include OVD-####, FVT-#### or No-JIRA.") 
