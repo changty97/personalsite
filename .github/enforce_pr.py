290a452f6104142994d5911cc05573d5a678e746
Tyler <43707450+changty97@users.noreply.github.com>
2022-11-09 16:12:34 -0800

    Update enforce_pr.py

--- .github/enforce_pr.py
+++ .github/enforce_pr.py
@@ -2,8 +2,12 @@ import os
 import sys
 import re
 
+JIRA = ["OVD", "FVT", "NO-JIRA"]
 
-pr_title = ["OVD", "FVT", "No-JIRA"]
-
-if not re.match(r"(?=("+'|'.join(pr_title)+r"))", os.environ.get('PR_TITLE')):
-  sys.exit("Pull Request Title MUST include OVD-####, FVT-#### or No-JIRA.") 
+# Check for any of the matching JIRA tags above in the PR Title environment variable
+# Regex List will be (OVD-(\d+): )|(FVT-(\d+): )|(NO-JIRA: )
+if not re.match(r"(" + r"-(\d+): )|(".join(JIRA) + ": )", os.environ.get('PR_TITLE')):
+    sys.exit(
+        "Pull Request Title MUST contain OVD or FVT with a dash and the Jira number, "
+        "or specify No-JIRA, then a colon and space."
+    )
