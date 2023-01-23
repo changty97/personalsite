e659c166b89a5f3f41b3cab3f91b189e0474a354
Tyler <43707450+changty97@users.noreply.github.com>
2023-01-23 10:04:42 -0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         <div className="center">
           <h2>BIO</h2>
             <p>
-              I am a Software Engineer at Intel in the Intel Foundry Services group. I like learning new technologies and growing my skill sets in areas like Artifical Intelligents (AI) and creating fun projects with Arduino. I recently graduated Fall of 2021 with a Bachelors Degree in Computer Science at California State University of Sacramento. For some of my latest projects checkout out my <a href="https://github.com/changty97" className="link">GitHub</a>.</p>
+              I am a Software Engineer at Intel in the Intel Foundry Services group. I like learning new technologies and growing my skill sets in areas like Artifical Intelligents (AI) and creating fun projects with Arduino. I recently graduated Fall of 2021 with a Bachelors Degree in Computer Science at California State University of Sacramento. For some of my latest projects checkout out my <a href="https://github.com/changty97" className="link">GitHub</a>.
             </p>
           //<p>I am a Software Engineer Intern at Intel where I automate and develop software on the Product Validation Tools team. Learning new technologies has always been my passion and has lead to gaining new skill sets such as problem solving. Currently, I am attending California State University of Sacramento where I expect to graduate Fall 2021. For some of my latest projects checkout out my <a href="https://github.com/changty97" className="link">GitHub</a>.</p>
         </div>
