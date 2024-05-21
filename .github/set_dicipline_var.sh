aca3f1db67af327901470664c1dde2b2c8361dda
Tyler <43707450+changty97@users.noreply.github.com>
2024-05-21 13:49:59 -0700

    Update set_dicipline_var.sh

--- .github/set_dicipline_var.sh
+++ .github/set_dicipline_var.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-DISCIPLINES = (
-  "circuit-marginality"
-  "customer-platform"
-  "design-for-debug"
-  "functional"
-  "power"
-  "performance"
-)
+DISCIPLINES = ( "circuit-marginality" "customer-platform" "design-for-debug" "functional" "power" "performance" )
 
 GET_DICIPLINE_CHANGES = $(git diff --name-only origin/main..$GITHUB_SHA | xargs dirname | sort | uniq)
 
