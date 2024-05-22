32b4b683d6a2fe217e31da140c52912628c31e33
Tyler <43707450+changty97@users.noreply.github.com>
2024-05-22 15:14:56 -0700

    Update set_dicipline_var.sh

--- .github/set_dicipline_var.sh
+++ .github/set_dicipline_var.sh
@@ -3,11 +3,10 @@
 DISCIPLINES=( "src" "circuit-marginality" "customer-platform" "design-for-debug" "functional" "power" "performance" )
 GET_DICIPLINE_CHANGES=$(git diff --name-only origin/main..$GITHUB_SHA | xargs dirname | sort | uniq | tr -d '\n')
 CURRENT_DISCIPLINE="N/A"
-
+echo $GET_DICIPLINE_CHANGES
 for i in "${DISCIPLINES[@]}"
 do
     if echo "$i" | grep -q "$GET_DICIPLINE_CHANGES"; then
-        echo $i
         $CURRENT_DISCIPLINE=$i
         break
     fi
