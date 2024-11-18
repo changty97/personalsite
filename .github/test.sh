b3741777ef1bd7b887fcc9fe07a93ad1080f601a
Tyler <43707450+changty97@users.noreply.github.com>
2024-11-18 14:43:00 -0800

    Update test.sh

--- .github/test.sh
+++ .github/test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-RETRIEVED_SECRET=$(echo "$test1" | base64 -di)
+RETRIEVED_SECRET="${{ needs.github-secrets.outputs.read-secret }}"
+echo "::add-mask::$RETRIEVED_SECRET"
+echo "secret=$RETRIEVED_SECRET" >> $GITHUB_OUTPUT
+echo "We retrieved our masked secret: $RETRIEVED_SECRET"
