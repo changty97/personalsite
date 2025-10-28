b962220990669bc953ef3edc34f9187d136a4391
Tyler <43707450+changty97@users.noreply.github.com>
2025-10-28 00:14:30 -0700

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo SECRET_VALUE="my_secret_token" >> $GITHUB_ENV
-echo "::add-mask::$SECRET_VALUE"
+#echo "::add-mask::$SECRET_VALUE"
 echo $SECRET_VALUE
 
 echo SECRET_VALUE2="my_secret_token2" >> $GITHUB_ENV
-echo "::add-mask::$SECRET_VALUE2"
+#echo "::add-mask::$SECRET_VALUE2"
 echo $SECRET_VALUE2
 
 if [[ -z "${SECRET_VALUE}" ]]; then
